c36babc505be8b78b75beb1c147c67a36b2db90d
Mark Small <m.small@ed.ac.uk>
2016-08-11 20:53:06 +0100

    Register User defined tasks:
    Add the ability for projects to register their own tasks with life-cycle
    phases.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
  * into its own file. Any files in that directory get automatically
  * required below.
  *
- * To add a new task, simply add a new task file that directory.
+ * To add a new task, simply add a new task file to that directory.
  * gulp/tasks/default.js specifies the default set of tasks to run
  * when you run `gulp`.
  *
@@ -15,6 +15,15 @@
 'use strict'
 
 import utils from './utils'
+
+import {AbstractTaskLoader} from './abstract-task-loader'
+// // module.exports = AbstractTaskLoader
+// module.exports = {
+// // export {
+// // export default {
+//   taskLoader: AbstractTaskLoader // ,
+// //   loader
+// }
 
 /**
  * This class takes care of loading gulp tasks.
@@ -33,11 +42,26 @@ class TasksLoader {
 
     gulp = utils.configureGulpObject(gulp, options) // we need to customize the gulp object a bit
 
-    utils.registerTask('./gulp/tasks/life-cycle', gulp)
-    utils.registerTask('./gulp/tasks/life-cycle/clean', gulp)
-    utils.registerTask('./gulp/tasks/life-cycle/default', gulp)
-    utils.registerTask('./gulp/tasks', gulp)
+    utils.registerTask('./gulp/tasks/life-cycle', gulp, options)
+    utils.registerTask('./gulp/tasks/life-cycle/clean', gulp, options)
+    utils.registerTask('./gulp/tasks/life-cycle/default', gulp, options)
+    utils.registerTask('./gulp/tasks', gulp, options)
+    utils.registerTask(options.tasks, gulp, options)
+    console.log(`INPUT OPTIONS: ${JSON.stringify(inputOptions)}, OPTIONS: ${JSON.stringify(options)}`)
   }
 }
 
 export default new TasksLoader()
+// let loader = new TasksLoader()
+// let taskLoader = new AbstractTaskLoader()
+
+export {
+// // export default {
+  AbstractTaskLoader // ,
+// //   loader
+}
+
+// module.exports = {
+//   taskLoader,
+//   loader
+// }
