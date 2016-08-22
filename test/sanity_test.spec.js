57719fc4f68dd41d66014498c192105136f7a645
Mark Small <m.small@ed.ac.uk>
2016-08-22 23:35:57 +0100

    Add simple test

---
+++ test/sanity_test.spec.js
@@ -0,0 +1,11 @@
+import {describe, it, xit, expect} from 'jasmine'
+
+describe('sanity checks', () => {
+  it('should be able to test', () => {
+    expect(true).toBe(true)
+  })
+
+  xit('should skip this', () => {
+    expect(4).toEqual(40)
+  })
+})
